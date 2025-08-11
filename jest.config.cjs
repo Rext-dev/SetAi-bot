8bae38b958f836ee2af013622a1a1eb1018560f9
Enrique <enrique.hernandez@jala.university>
2025-08-11 15:37:19 -0600

    feat: configurar ts y jest

---
+++ jest.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/bot"],
+  testMatch: ["**/*.test.ts"],
+  moduleFileExtensions: ["ts", "js", "json"],
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      { tsconfig: "<rootDir>/tsconfig.jest.json", useESM: true },
+    ],
+  },
+  extensionsToTreatAsEsm: [".ts"],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      lines: 70,
+      statements: 70,
+      functions: 70,
+      branches: 50,
+    },
+  },
+  collectCoverageFrom: ["bot/src/**/*.ts", "!bot/src/**/*.d.ts"],
+  coverageDirectory: "coverage",
+};
